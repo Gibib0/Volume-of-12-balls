a125f5437a701fdbd496c3acafabba75fa737d43
Anastasia <anast111antonova@gmail.com>
2025-08-21 18:27:40 +0300

    add calculation of internal volume by formula

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 'use sctrict'
 
+// Inner volume of sphere equals 4/3 * Pi * R^3
+
 const n = 12
 const wallThickness = 0.01
 
 let outerDiameter = 0.5
 
 let totalVolume = 0
+
+for (let i = 1; i <= n; i++) {
+	let outerRadius = outerDiameter / 2
+	let innerRadius = outerRadius - wallThickness
+
+	let volume = (4 / 3) * Math.PI * (innerRadius ** 3)
+	totalVolume += volume
+
+	outerDiameter = innerRadius * 2
+}
+
+console.log('Total internal volume of 12 balls equals:', totalVolume, 'm^3');

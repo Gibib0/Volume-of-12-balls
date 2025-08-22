7337ede791073b86926c58364e2a0c946184bf67
Anastasia <anast111antonova@gmail.com>
2025-08-22 18:22:11 +0300

    Fix variables names; fix bugs

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-'use sctrict'
+'use strict'
 
-const n = 12
-const wallThickness = 0.01
-
-let outerDiameter = 0.5
+const N = 12
+const WALL_THICKNESS = 0.01
+const SMALLEST_OUTER_DIAMETER = 0.5
 
 let totalVolume = 0
 
-for (let i = 1; i <= n; i++) {
-	let outerRadius = outerDiameter / 2
-	let innerRadius = outerRadius - wallThickness
+let outerDiameter = SMALLEST_OUTER_DIAMETER
+
+for (let i = 1; i <= N; i++) {
+	let innerRadius = outerDiameter / 2 - WALL_THICKNESS
 
 	let volume = (4 / 3) * Math.PI * (innerRadius ** 3)
 	totalVolume += volume
 
-	outerDiameter = innerRadius * 2
+	outerDiameter += WALL_THICKNESS * 2
 }
 
 console.log('Total internal volume of 12 balls equals:', totalVolume, 'm^3');

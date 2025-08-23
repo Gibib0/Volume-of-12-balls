7c0286db0788c130b92f09f3119d597e8470dd3e
Anastasia <anast111antonova@gmail.com>
2025-08-23 17:22:44 +0300

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -6,15 +6,13 @@ const SMALLEST_OUTER_DIAMETER = 0.5
 
 let totalVolume = 0
 
-let outerDiameter = SMALLEST_OUTER_DIAMETER
+let innerRadius = SMALLEST_OUTER_DIAMETER / 2 - WALL_THICKNESS
 
 for (let i = 1; i <= N; i++) {
-	let innerRadius = outerDiameter / 2 - WALL_THICKNESS
-
 	let volume = (4 / 3) * Math.PI * (innerRadius ** 3)
 	totalVolume += volume
 
-	outerDiameter += WALL_THICKNESS * 2
+	innerRadius += WALL_THICKNESS
 }
 
 console.log('Total internal volume of 12 balls equals:', totalVolume, 'm^3');
